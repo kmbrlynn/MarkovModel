b41d9c244ca591f671ba2fad4f1ce97632c86b57
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-10 14:37:18 -0400

    stub em out

--- MarkovModel.cpp
+++ MarkovModel.cpp
@@ -1,2 +1,44 @@
 // Copyright Kim Doglas 2015
+#include <map>
+#include <string>
+#include "MarkovModel.hpp"
+
+// ============================================================ con/destructors
+MarkovModel::MarkovModel(std::string text, int k) {
+
+
+
+
+}
+
+MarkovModel::~MarkovModel() {
+}
+
+// ================================================================== accessors
+int MarkovModel::order() {
+
+}
+
+int MarkovModel::freq(std::string kgram) {
+
+}
+
+int MarkovModel::freq(std::string kgram, char c) {
+
+}
+
+char MarkovModel::randk(std::string kgram) {
+
+}
+
+std::string MarkovModel::gen(std::string kgram, int t) {
+
+}
+
+// ==================================================================== friends
+std::ostream& operator << (std::ostream& os, MarkovModel& mm) {
+
+
+}
+
 
--- MarkovModel.hpp
+++ MarkovModel.hpp
@@ -1,1 +1,22 @@
 // Copyright Kim Douglas 2015
+#include <map>
+#include <string>
+
+class MarkovModel {
+ public:
+	MarkovModel(std::string text, int k);
+	~MarkovModel();f
+	
+	int order();
+	int freq(std::string kgram);
+	int freq(std::string kgram, char c);
+	char randk(std::string kgram);
+	std::string gen(std::string kgram, int T);
+	
+	friend std::ostream& operator <<(std::ostream& os, MarkovModel& mm);
+
+ private:
+	int _order;
+	map <std::string, int> _kgrams;
+	std::string _alphabet;
+};
