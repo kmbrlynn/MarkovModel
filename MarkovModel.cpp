e163395449c608d9fb9752ab53415286d34ac826
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-12 13:44:55 -0400

    wraparound in constructor

--- MarkovModel.cpp
+++ MarkovModel.cpp
@@ -4,9 +4,25 @@
 #include "MarkovModel.hpp"
 
 // ============================================================ con/destructors
-MarkovModel::MarkovModel(std::string text, int k) {
+MarkovModel::MarkovModel(std::string text, int k) : 
+                                    _alphabet(text), _order(k) {
+    std::string kgram_str, wrap_str;
+    int kgram_back, wrap_back;
 
+    for (unsigned int i = 0; i < _alphabet.length(); i++) {
+        kgram_back = i+k-1;
+        if (kgram_back >= _alphabet.length()) { 
+            // grab the wraparound chunk
+            wrap_back = kgram_back - _alphabet.length();
+            wrap_str = _alphabet.substr(0, wrap_back);
+            // add it to the truncated kgram
+            kgram_str = _alphabet.substr(i, _alphabet.length()-1);
+            kgram_str.append(wrap_str);
+        } else { 
+            kgram_str = _alphabet.substr(i, kgram_back);
+        }
 
+    }
 
 
 }
@@ -16,7 +32,7 @@ MarkovModel::~MarkovModel() {
 
 // ================================================================== accessors
 int MarkovModel::order() {
-
+    return _order;
 }
 
 int MarkovModel::freq(std::string kgram) {
