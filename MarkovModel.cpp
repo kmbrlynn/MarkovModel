d8741bc7d1750c4e52a206c8a9b2b802585862fe
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-12 14:55:36 -0400

    add-to-map functionality in constructor

--- MarkovModel.cpp
+++ MarkovModel.cpp
@@ -6,22 +6,33 @@
 // ============================================================ con/destructors
 MarkovModel::MarkovModel(std::string text, int k) : 
                                     _alphabet(text), _order(k) {
+                  
     std::string kgram_str, wrap_str;
     int kgram_back, wrap_back;
 
     for (unsigned int i = 0; i < _alphabet.length(); i++) {
         kgram_back = i+k-1;
+
+        // Get the current kgram
         if (kgram_back >= _alphabet.length()) { 
             // grab the wraparound chunk
             wrap_back = kgram_back - _alphabet.length();
             wrap_str = _alphabet.substr(0, wrap_back);
-            // add it to the truncated kgram
+            // append to the truncated kgram
             kgram_str = _alphabet.substr(i, _alphabet.length()-1);
             kgram_str.append(wrap_str);
         } else { 
             kgram_str = _alphabet.substr(i, kgram_back);
-        }
+        }        
 
+        // Put it in the map
+        if (_kgrams.find(kgram_str) == _kgrams.end())
+        {
+            if (kgram_back == _alphabet.length()-1)
+                _kgrams[kgram_str] = _alphabet.at(0);
+            else
+                _kgrams[kgram_str] = _alphabet.at(kgram_back+1);
+        }
     }
 
 
