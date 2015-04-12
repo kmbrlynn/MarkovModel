12f6b604a2243fdde7b70d36c6aa0b959fca2438
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-12 17:16:54 -0400

    rename from _alphabet to _alpha

--- MarkovModel.hpp
+++ MarkovModel.hpp
@@ -4,19 +4,19 @@
 
 class MarkovModel {
  public:
-	MarkovModel(std::string text, int k);
-	~MarkovModel();
-	
-	int order();
-	int freq(std::string kgram);
-	int freq(std::string kgram, char c);
-	char randk(std::string kgram);
-	std::string gen(std::string kgram, int T);
-	
-	friend std::ostream& operator <<(std::ostream& os, MarkovModel& mm);
+    MarkovModel(std::string text, int k);
+    ~MarkovModel();
+    
+    int order();
+    int freq(std::string kgram);
+    int freq(std::string kgram, char c);
+    char randk(std::string kgram);
+    std::string gen(std::string kgram, int T);
+    
+    friend std::ostream& operator <<(std::ostream& os, MarkovModel& mm);
 
  private:
-	int _order;
-	std::map <std::string, int> _kgrams;
-	std::string _alphabet;
+    int _order;
+    std::map <std::string, int> _kgrams;
+    std::string _alpha;
 };
