13a2af1eedb1130a785809bb3deadb9a1b87283d
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-14 14:42:41 -0400

    add kplus to constructor

--- MarkovModel.cpp
+++ MarkovModel.cpp
@@ -11,40 +11,54 @@ std::string freq_err = "freq(string, [int]): kgram length musn't exceed order";
 
 // ============================================================ con/destructors
 MarkovModel::MarkovModel(std::string text, int k) : _order(k), _alpha(text) { 
-    std::string kgram_str, wrap_str;
-    int kgram_back, wrap_back;
+    int kgram_back, kplus_back;
 
     // Parse the alphabet out of the string of text
     std::sort(_alpha.begin(), _alpha.end());
     _alpha.erase(std::unique(_alpha.begin(), _alpha.end()), _alpha.end());
 
-    // assign each unique _alpha character a 'tally' variable
-    // so, maintain 3 for this example - a, g, c
-    // check against each tally var when you have a kgram collision
-    // store in member vector?
-
-
+    // build k-grams and k-plus-one-grams
     for (unsigned int i = 0; i < text.length(); i++) {
+        std::string kgram_str, kplus_str, wrap_str;
+        int wrap_back;
+        
         kgram_back = i+k;
-
-        // Get the current kgram
+        kplus_back = kgram_back + 1;
+       
+        // Get current kgram: wraparound + append, if necessary
+        // then put it in the map, or tally existing one
         if (kgram_back >= text.length()) { 
-            // grab the wraparound chunk
             wrap_back = kgram_back - text.length();
             wrap_str = text.substr(0, wrap_back);
-            // append to the truncated kgram
             kgram_str = text.substr(i, text.length()-1);
-            kgram_str.append(wrap_str);
-        } else { 
-            kgram_str = text.substr(i, k);
-        }        
-
-        // Put it in the map, or tally the existing one
-        std::map<std::string, int>::iterator it = _kgrams.find(kgram_str);
-        if (it != _kgrams.end())
-            it-> second += 1;
-        else
+            kgram_str.append(wrap_str); 
+        } else kgram_str = text.substr(i, k);       
+        std::cout << "got kgram: " << kgram_str << std::endl;
+        
+        std::map<std::string, int>::iterator kg_it = _kgrams.find(kgram_str);
+        if (kg_it != _kgrams.end()) {
+            kg_it-> second += 1;
+            std::cout << "tallied kgram: " << kgram_str << std::endl;
+        } else {
             _kgrams[kgram_str] = 1;
+            std::cout << "created kgram " << kgram_str << std::endl; 
+        }
+
+        // Get current kplus-one-gram: based on current kgram
+        // then put it in the map, or tally existing one
+        if (kplus_back == text.length()) kplus_back = text.at(0);   
+        kplus_str = kgram_str + text.at(kplus_back);
+        std::cout << "got kplus: " << kplus_str << std::endl;
+
+        std::map<std::string, int>::iterator kp_it = _kgrams.find(kplus_str);
+        if (kp_it != _kgrams.end()) {
+            kp_it-> second += 1;
+            std::cout << "tallied kplus: " << kgram_str << std::endl;
+        } else {
+            _kgrams[kplus_str] = 1;
+            std::cout << "created kplus " << kplus_str << std::endl;
+        }
+        std::cout << std::endl;
     }
 }
 
