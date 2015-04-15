6feee4d1157ec6975ca616b4df7aec33c5e06c28
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-15 09:20:41 -0400

    cpplinted it

--- TextGenerator.cpp
+++ TextGenerator.cpp
@@ -1,19 +1,14 @@
+// Copyright 2015 Kim Douglas
 #include <time.h>
 #include <stdlib.h>
 #include <iostream>
 #include "MarkovModel.hpp"
 
 int main(int argc, char* argv[]) {
-   
-  //  MarkovModel mm("abcdefgh", 3);
- 
     srand(time(NULL));
 
     MarkovModel mm("gagggagaggcgagaaa", 1);
     std::cout << mm << std::endl;
 
-//    char randchar = mm.randk("a");
-  //  std::cout << "rand char = " << randchar << std::endl;
-
-	return 0;
+    return 0;
 }
