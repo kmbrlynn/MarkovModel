82456c2adf5620346c7bfd8a314b4f5cb285498c
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-14 22:11:05 -0400

    begin randk() function

--- MarkovModel.cpp
+++ MarkovModel.cpp
@@ -1,6 +1,6 @@
 // Copyright Kim Douglas 2015
 #include <map>
-#include <cmath>
+#include <vector>
 #include <exception>
 #include <stdexcept>
 #include <iostream>
@@ -22,13 +22,12 @@ MarkovModel::MarkovModel(std::string text, int k) : _order(k), _alpha(text) {
     std::sort(_alpha.begin(), _alpha.end());
     _alpha.erase(std::unique(_alpha.begin(), _alpha.end()), _alpha.end());
 
-    // build k-grams and k-plus-one-grams
+    // Build k-grams and k-plus-one-grams
     for (unsigned int i = 1; i < text.length()+1; i++) {
         std::string kgram_str, kplus_str, wrap_str;
         int wrap_len;
 
-        kgram_back = i+k-1;
-  //      std::cout << "kgram_back = " << kgram_back << std::endl;
+        kgram_back = i+k-1;  // this is why we start index at 1
        
         // Get current kgram: wraparound + append, if necessary
         // Get current kplus: based on kgram
@@ -103,7 +102,6 @@ int MarkovModel::freq(std::string kgram, char c) {
 
     std::string str(1, c);
     std::string kplus = kgram + str;
-
     std::map<std::string, int>::iterator it = _kgrams.find(kplus);
     if (it != _kgrams.end()) {
         return it-> second;
@@ -113,6 +111,14 @@ int MarkovModel::freq(std::string kgram, char c) {
 }
 
 char MarkovModel::randk(std::string kgram) {
+    // find the total number of kgrams
+    int kgram_freq = freq(kgram);
+   
+    // find the total numbers of each of the k-plus-one-grams 
+    std::vector<int> kplus_freqs;
+    for (unsigned int i = 0; i < _alpha.length(); i++) {
+        
+    }
 
 }
 
